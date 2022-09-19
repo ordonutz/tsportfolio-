56711166076f9fff02129ddbb283bcbe459e4aa3
Leslie Ordonez <104482135+ordonutz@users.noreply.github.com>
2022-09-19 00:19:48 -0700

    updated layout still needs some work and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
   Footer,
   MediaQuery,
   Burger,
-  useMantineTheme
+  useMantineTheme,
 } from "@mantine/core";
 import MainLogo from "./assets/MainLogo";
 import NavLinks from "./components/NavLinks";
@@ -15,59 +15,50 @@ import HomePage from "./components/Pages/HomePage";
 import ResumePage from "./components/Pages/ResumePage";
 import ProjectPage from "./components/Pages/ProjectPage";
 import ContactPage from "./components/Pages/ContactPage";
+import { HeaderResponsive } from "./components/HeaderResponsive/index";
+import { FooterResponsive } from "./components/FooterResponsive";
 
 function App() {
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
-  
+  const temp = [
+    { link: "link", label: "Projects" },
+    { link: "link", label: "Resume" },
+    { link: "link", label: "Contact" },
+  ];
 
+  // return <HeaderResponsive links={temp} />;
   return (
     <AppShell
       styles={{
         main: {
-          background:
-            "linear-gradient(180deg, #989898 0%, #CBC8C8 14.58%, #E3DFDF 25.52%, #F0EBEB 31.77%, #D5D1D1 44.79%, #DAD7D7 58.85%, #E2DEDE 74.48%, #B3B3B3 94.27%)"
+          background: "white",
         },
       }}
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       footer={
-        <Footer height={60} p="md" style={{backgroundColor: "transparent"}} withBorder={false}>
-          Application footer
-        </Footer>
+        // <Footer
+        //   height={60}
+        //   p="md"
+        //   style={{ backgroundColor: "transparent" }}
+        //   withBorder={false}
+        // >
+        //   Application footer
+        // </Footer>
+        <FooterResponsive links={temp} />
       }
-      header={
-        
-        <Header height={90} p="md" withBorder= {false} style={{backgroundColor: "transparent"}}>
-         <div style={{  display: 'flex', flexDirection: 'row', justifyContent: 'space-between' ,alignItems: 'center', height: '100%', paddingLeft: '5%', paddingRight:'5%'}}>
-            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-           <MainLogo />
-           <NavLinks/>
-          
-            
-         </div> 
-        </Header>
-      }
+      header={<HeaderResponsive links={temp} />}
+      // header={<Header children={<div>hi</div>} height="10" />}
     >
-      <div style={{paddingLeft: '5%', paddingRight: '5%', background: "red"}}>
-
-      <HomePage/>
-      
+      {/* <div style={{ paddingLeft: "5%", paddingRight: "5%", background: "red" }}> */}
+      <HomePage />
+      {/* </div> */}
+      {/* <div style={{ background: "green", width: "auto" }}>
+        <ResumePage />
       </div>
-      <div style={{background: "green", width: "auto"}}>
-      <ResumePage/>
-  
-      </div>
-      <ProjectPage/>
-      <ContactPage/>
+      <ProjectPage />
+      <ContactPage /> */}
     </AppShell>
   );
 }
---
+++ src/components/FooterResponsive/index.tsx
@@ -0,0 +1,57 @@
+import { createStyles, Container, Group, Anchor } from "@mantine/core";
+import MainLogo from "../../assets/MainLogo";
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: 20,
+  },
+
+  inner: {
+    maxWidth: "1440px",
+    background: "red",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column",
+    },
+  },
+
+  links: {
+    [theme.fn.smallerThan("xs")]: {
+      marginTop: theme.spacing.md,
+    },
+  },
+}));
+
+interface FooterSimpleProps {
+  links: { link: string; label: string }[];
+}
+
+export function FooterResponsive({ links }: FooterSimpleProps) {
+  const { classes } = useStyles();
+
+  const items = links.map((link) => (
+    <Anchor<"a">
+      color="dimmed"
+      key={link.label}
+      href={link.link}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div style={{ background: "blue" }} className={classes.footer}>
+      <Container className={classes.inner}>
+        <MainLogo />
+        <Group className={classes.links}>{items}</Group>
+      </Container>
+    </div>
+  );
+}
