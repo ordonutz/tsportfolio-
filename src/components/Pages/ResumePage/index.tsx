37905272aa81dcacc92a552f985808e1da9d0440
Leslie Ordonez <104482135+ordonutz@users.noreply.github.com>
2022-09-22 23:10:39 -0700

    basic resume section layout done need to add content and fix issue with styling on main section

--- src/components/Pages/ResumePage/index.tsx
+++ src/components/Pages/ResumePage/index.tsx
@@ -38,38 +38,45 @@ import LinkedIcon from "../../../assets/LinkedIcon";
 import MailIcon from "../../../assets/MailIcon";
 import PhoneIcon from "../../../assets/PhoneIcon";
 
-const mockdata = [
+interface resumeSectionData {
+  title: string;
+  subTitles: Array<string>;
+  dates: Array<string>;
+  descriptions: Array<string>;
+}
+
+const temp = [
   {
-    title: "Top 10 places to visit in Norway this summer",
-    image:
-      "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-    date: "August 18, 2022",
+    title: "Education",
+    subsection1: {
+      name: "University of Nevada Las Vegas, Bachelor of Science in Computer Science",
+      date: "",
+      description: "",
+    },
   },
-  {
-    title: "Best forests to visit in North America",
-    image:
-      "https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-    date: "August 27, 2022",
-  },
-  {
-    title: "Hawaii beaches review: better than you think",
-    image:
-      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-    date: "September 9, 2022",
-  },
-  {
-    title: "Mountains at night: 12 best locations to enjoy the view",
-    image:
-      "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
-    date: "September 12, 2022",
-  },
+  { title: "Experience" },
+  { title: "Projects" },
+  { title: "Leadership Experience" },
+];
+
+const resumeSectionTitle = [
+  "Education",
+  "Experience",
+  "Projects",
+  "Leadership Experience",
+];
+
+const resumeSectionSubtitles = [
+  "University of Nevada Las Vegas, Bachelor of Science in Computer Science",
+  "College of Southern Nevada, Associate of Science",
+  "Rensselaer Polytechnic Institute Troy, NY",
 ];
 
 const useStyles = createStyles((theme) => ({
   card: {
     background: theme.colors.pink[2],
     transition: "transform 150ms ease, box-shadow 150ms ease",
-    width: "80%",
+    width: "90%",
     margin: "1% auto",
 
     "&:hover": {
@@ -88,7 +95,7 @@ const useStyles = createStyles((theme) => ({
     shadow: "xl",
     height: "25%",
     background: theme.colors.gray[2],
-    width: "60%",
+    width: "70%",
     margin: "7% auto",
   },
   cardGrid: {},
@@ -100,24 +107,10 @@ const useStyles = createStyles((theme) => ({
 export function ResumePage() {
   const { classes } = useStyles();
   const theme = useMantineTheme();
-  const cards = mockdata.map((article) => (
-    <Card
-      key={article.title}
-      p="md"
-      radius="md"
-      component="a"
-      href="#"
-      className={classes.card}
-    >
-      <AspectRatio ratio={1920 / 1080}>
-        <Image src={article.image} />
-      </AspectRatio>
-      <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
-        {article.date}
-      </Text>
-      <Text className={classes.title} mt={5}>
-        {article.title}
-      </Text>
+
+  const cards = resumeSectionTitle.map((title) => (
+    <Card key={title} p="sm" radius="md" component="a" className={classes.card}>
+      <Text>{title}</Text>
     </Card>
   ));
 
