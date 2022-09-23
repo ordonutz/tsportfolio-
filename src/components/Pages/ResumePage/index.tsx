519d9cbdf20d141f98cc1b2211a565e39c7e9767
Leslie Ordonez <104482135+ordonutz@users.noreply.github.com>
2022-09-23 00:26:05 -0700

    populated sections with content next need to add classNames and styling

--- src/components/Pages/ResumePage/index.tsx
+++ src/components/Pages/ResumePage/index.tsx
@@ -38,38 +38,107 @@ import LinkedIcon from "../../../assets/LinkedIcon";
 import MailIcon from "../../../assets/MailIcon";
 import PhoneIcon from "../../../assets/PhoneIcon";
 
+interface resumeSubSection {
+  name: string;
+  date: string;
+  description: string | Array<string>;
+}
 interface resumeSectionData {
   title: string;
-  subTitles: Array<string>;
-  dates: Array<string>;
-  descriptions: Array<string>;
+  subsection0: resumeSubSection;
+  subsection1?: resumeSubSection;
+  subsection2?: resumeSubSection;
 }
 
-const temp = [
+const resumeData: Array<resumeSectionData> = [
   {
     title: "Education",
-    subsection1: {
+    subsection0: {
       name: "University of Nevada Las Vegas, Bachelor of Science in Computer Science",
-      date: "",
-      description: "",
+      date: "December 2021",
+      description: "GPA: 3.65, Minor in mathematical sciences",
+    },
+    subsection1: {
+      name: "College of Southern Nevada, Associate of Science",
+      date: "December 2019",
+      description:
+        "GPA: 3.32, University transfer program with an emphasis on computer science fundamentals",
+    },
+    subsection2: {
+      name: "Rensselaer Polytechnic Institute Troy, NY",
+      date: "August 2016 - May 2017",
+      description:
+        "Completed 21 credits towards a B.S. in Chemical Engineering",
     },
   },
-  { title: "Experience" },
-  { title: "Projects" },
-  { title: "Leadership Experience" },
-];
-
-const resumeSectionTitle = [
-  "Education",
-  "Experience",
-  "Projects",
-  "Leadership Experience",
-];
-
-const resumeSectionSubtitles = [
-  "University of Nevada Las Vegas, Bachelor of Science in Computer Science",
-  "College of Southern Nevada, Associate of Science",
-  "Rensselaer Polytechnic Institute Troy, NY",
+  {
+    title: "Experience",
+    subsection0: {
+      name: "Developer II, Credit One Bank, Las Vegas, NV",
+      date: "December 2021 - June 2022",
+      description: [
+        "Collaborated in an agile team of 9 for front-end web development using React.js, JavaScript, TypeScript, Sass, and internal UI Framework ",
+        "Implemented functional components, custom hooks, and REST API calls for a page within an internal tool app ",
+        "Maintained code through GitLab, code reviews, and at least 80% coverage using Jest unit testing framework ",
+      ],
+    },
+    subsection1: {
+      name: "Computer Science I and II Teaching Assistant, C++, UNLV",
+      date: "August 2020 - August 2021",
+      description: [
+        "Graded over 90% of course material for undergraduate classes of  25+ students",
+        "Reviewed and tested 8 - 10 C++ programming assignments for each course in a Linux environment",
+      ],
+    },
+    subsection2: {
+      name: "Teaching Assistant, NSA/NSF Sponsored GenCyber Camp, UNLV",
+      date: "July 2021 - August 2021",
+      description: [
+        "Mentored a group of 8 high school students to assist with 10+ hands-on cyber security labs using Kali Linux",
+        "Coordinated with camp staff daily to structure labs, activities, and smooth running of the summer camp",
+      ],
+    },
+  },
+  {
+    title: "Projects",
+    subsection0: {
+      name: "Portfolio Website, React.js, TypeScript, Mantine Component Library",
+      date: "September 2022",
+      description:
+        "Designed and developed a responsive personal website with Mantine UI",
+    },
+    subsection1: {
+      name: "Simulating Game of Pickup Sticks, C++",
+      date: "December 2019",
+      description:
+        "Modified Depth First Search and implemented topological sort to determine if a solution existed through cycle detection and efficient searching",
+    },
+  },
+  {
+    title: "Leadership Experience",
+    subsection0: {
+      name: "Credit One Bank College Programs and Recruitment",
+      date: "February 2022",
+      description: [
+        "Traveled with HR to the University of AZ and University of UT to assist with recruitment at the career fair booths",
+        "Volunteered and collaborated with colleagues at the Las Vegas Rescue Mission to serve over 200 meals",
+      ],
+    },
+    subsection1: {
+      name: "IntelliSys 2021 Paper published by Springer, Global Postal Automation",
+      date: "August 2021",
+      description:
+        "Co-authored and presented an undergrad research paper at the virtual IntelliSys conference",
+    },
+    subsection2: {
+      name: "Lead Designer, Customisitc",
+      date: " February 2018 - February 2020",
+      description: [
+        "Managed and trained a team of 5 employees and facilitated over 40% of all client transactions",
+        "Became proficient with Flexi design software and other custom print technologies on an as-needed basis",
+      ],
+    },
+  },
 ];
 
 const useStyles = createStyles((theme) => ({
@@ -108,9 +177,37 @@ export function ResumePage() {
   const { classes } = useStyles();
   const theme = useMantineTheme();
 
-  const cards = resumeSectionTitle.map((title) => (
-    <Card key={title} p="sm" radius="md" component="a" className={classes.card}>
-      <Text>{title}</Text>
+  const cards = resumeData.map((section) => (
+    <Card
+      key={section.title}
+      p="sm"
+      radius="md"
+      component="a"
+      className={classes.card}
+    >
+      <Text>{section.title}</Text>
+      <Text>{section.subsection0?.name}</Text>
+      <Text>{section.subsection0?.date}</Text>
+
+      {Array.isArray(section.subsection0?.description) ? (
+        section.subsection0?.description.map((temp: string) => <li>{temp}</li>)
+      ) : (
+        <li>{section.subsection0?.description}</li>
+      )}
+      <Text>{section.subsection1?.name}</Text>
+      <Text>{section.subsection1?.date}</Text>
+      {Array.isArray(section.subsection1?.description) ? (
+        section.subsection1?.description.map((temp: string) => <li>{temp}</li>)
+      ) : (
+        <li>{section.subsection1?.description}</li>
+      )}
+      <Text>{section.subsection2?.name}</Text>
+      <Text>{section.subsection2?.date}</Text>
+      {Array.isArray(section.subsection2?.description) ? (
+        section.subsection2?.description.map((temp: string) => <li>{temp}</li>)
+      ) : (
+        <li>{section.subsection2?.description}</li>
+      )}
     </Card>
   ));
 
@@ -119,7 +216,7 @@ export function ResumePage() {
       <ScrollArea style={{ height: "100%" }} type="always" scrollbarSize={20}>
         <Group className={classes.resumeHeader} position="apart" spacing="md">
           <Title color={theme.colors.gray[7]} classNames={classes.title}>
-            Leslie Ordonez
+            Leslie Ordonez-Hernandez
           </Title>
           <Group>
             <PhoneIcon />
